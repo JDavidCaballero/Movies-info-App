6428e21e13882a6e6260d1fff23d745b8629ee87
Juan David Caballero <58749835+JDavidCaballero@users.noreply.github.com>
2022-06-12 20:53:12 -0500

    Comment night mode

--- app/src/main/java/com/jdcm/testserviinfoapp/ui/movies/MoviesFragment.kt
+++ app/src/main/java/com/jdcm/testserviinfoapp/ui/movies/MoviesFragment.kt
@@ -64,6 +64,8 @@ class MoviesFragment : Fragment() {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 if (s != null && s.toString().isNotEmpty()) {
+
+                    //No planned functionality Search Filter
                     searchFilter(s.toString())
                 } else {
                     binding.searchEd.clearFocus()
